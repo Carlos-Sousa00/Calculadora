8a732ffde330408fad6b71e0fc54ee575456ff16
Carlos Sousa <carlos.sousa@sintese.local>
2022-10-10 12:51:49 -0300

    EstudosNoTrabalho

---
+++ Apis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Apis
+{
+    class Program  
+    {
+        static void Main (string[] args)
+        {
+
+        bool teste = true;
+        short min = 15;
+        float primeira = 15.5f;
+        decimal primeiraD= 15.5m;
+        Console.WriteLine("Hello, World!");
+        char primeiraLetra = 'D';
+        var segundaLetra = 'O';
+        string texto = "É preciso utilizar aspas duplas";
+        int? idade = null;
+        int inteiro = 100;
+        Console.WriteLine(inteiro);
+        float real = 25.5f;
+        Console.WriteLine (real);
+        inteiro = (int)real;
+        Console.WriteLine(inteiro);
+
+        }
+    }
+}
+
+  
