d2ef43dbe26fc4d4d661730f7bba798cd2b06249
Carlos Sousa <carlos.sousa@sintese.local>
2022-10-10 16:17:01 -0300

    anotacoes

--- Apis/Program.cs
+++ Apis/Program.cs
@@ -7,7 +7,7 @@ namespace Apis
         static void Main (string[] args)
         {
 
-        bool teste = true;
+        /**bool teste = true;
         short min = 15;
         float primeira = 15.5f;
         decimal primeiraD= 15.5m;
@@ -15,14 +15,20 @@ namespace Apis
         char primeiraLetra = 'D';
         var segundaLetra = 'O';
         string texto = "É preciso utilizar aspas duplas";
-        int? idade = null;
+        int? idade = null;**/
         int inteiro = 100;
         Console.WriteLine(inteiro);
-        float real = 25.5f;
+        float real = 25.6f;
         Console.WriteLine (real);
         inteiro = (int)real;
         Console.WriteLine(inteiro);
+        string valorReal = real.ToString();
+        //inteiro = int.Parse(real.ToString()); // ou inteiro = int.Parse(valorReal);
+        Console.WriteLine(inteiro);
+        Console.WriteLine(valorReal);
 
+        inteiro = Convert.ToInt32(real);
+        Console.WriteLine(inteiro);
         }
     }
 }
