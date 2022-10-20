eeefd6fcb17766ccaa968b96c0500d643068738a
Carlos Sousa <carlos.sousa@sintese.local>
2022-10-20 17:16:19 -0300

    Hoje rendeu

--- Apis/Program.cs
+++ Apis/Program.cs
@@ -22,7 +22,47 @@ namespace Apis
         Console.WriteLine (real);
         inteiro = (int)real;
         Console.WriteLine(inteiro);
+
+        var arr = new string [2];
+        arr [0] = "Item 1";
+        var arr2 = arr;
+
+        Console.WriteLine(arr[0]);
+        Console.WriteLine(arr2[0]);
+
+        arr2[0] = "Item alterado";
+        Console.WriteLine(arr[0]);
+        Console.WriteLine(arr2[0]);
+
+        Product mouse = new Product ();
         
+        Analistas analista1 = new Analistas();
+
+        analista1.Name=("Yago");
+        analista1.Phone = 000000000;
+        Console.WriteLine(analista1.Name, " and ", analista1.Phone);
+        }
+    }
+
+    struct Analistas
+    {
+        public string Name;
+        public int Phone;
+    }
+    struct Product
+    {
+        public Product(int id, string name, double price)
+        {
+            Id=id;
+            Name=name;
+            Price=price;
+        }
+        public int Id;
+        public string Name;
+        public double Price;
+        public double PriceInDolar (double dolar)
+        {
+            return Price * dolar;
         }
     }
 }
