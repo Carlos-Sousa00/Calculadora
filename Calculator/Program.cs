1ae890a02ed5b25bf17de5afcbc689fd3741ecae
Carlos Sousa <carlos.sousa@sintese.local>
2022-11-29 12:00:45 -0300

    adição de funções

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,14 @@ namespace Calculator
     {
         static void Main (string [] args)
         {
+            Subtracao();
+           
+        }
+
+        static void Soma(){
+
             Console.Clear();
+
             Console.WriteLine("Primeiro valor: ");
             float v1 = float.Parse(Console.ReadLine());
 
@@ -17,7 +24,24 @@ namespace Calculator
 
             //INTERPOLAÇÃO DE STRING           
             Console.WriteLine($"O resultado a soma é {v1 + v2}");
-           
+
+            Console.ReadKey();
+        }
+
+        static void Subtracao(){
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor");
+            float v1 = float.Parse (Console.ReadLine());
+
+            Console.WriteLine("Segundo valor");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"O resultado da subtração é {v1-v2}");
+
+            Console.ReadKey();
         }
     }
 }
